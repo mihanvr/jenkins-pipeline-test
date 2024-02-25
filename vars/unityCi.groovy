2bfb278bb04624d1221f23f5bc2f3e5c74f1769f
Miha <mihanvr@gmail.com>
2024-02-25 18:21:01 +0300

    test

--- vars/unityCi.groovy
+++ vars/unityCi.groovy
@@ -4,10 +4,10 @@ def call(def options) {
 
 def pipeline(def options) {
     try {
-        discordPush(discordUrl: "https://discord.com/api/webhooks/1009734622650834994/RKxPLNbHgfO2JFQFY0CR7u2yCYgdzF71R-9JVt7h2L-LOxs83t77X5XY_wlYJqqz7Edl", message: [embeds: [[color = 3506169, fields = [[name: "1", value: "2", inline: true]]]]])
+        discordPush(webhookUrl: "https://discord.com/api/webhooks/1009734622650834994/RKxPLNbHgfO2JFQFY0CR7u2yCYgdzF71R-9JVt7h2L-LOxs83t77X5XY_wlYJqqz7Edl", message: [embeds: [[color = 3506169, fields = [[name: "1", value: "2", inline: true]]]]])
     } catch (Exception e) {
         try {
-            discordPush(discordUrl: "https://discord.com/api/webhooks/1009734622650834994/RKxPLNbHgfO2JFQFY0CR7u2yCYgdzF71R-9JVt7h2L-LOxs83t77X5XY_wlYJqqz7Edl", message: [embeds: [[color = 14225172, content = e.toString(), fields = [[name: "1", value: "2", inline: true]]]]])
+            discordPush(webhookUrl: "https://discord.com/api/webhooks/1009734622650834994/RKxPLNbHgfO2JFQFY0CR7u2yCYgdzF71R-9JVt7h2L-LOxs83t77X5XY_wlYJqqz7Edl", message: [embeds: [[color = 14225172, content = e.toString(), fields = [[name: "1", value: "2", inline: true]]]]])
         } catch (Exception e2) {
             echo e2.toString()
         }
@@ -60,6 +60,8 @@ def discordPush(def options) {
         def webhookUrl = options.webhookUrl
         def message = options.message
         def json = writeJSON(json: message, returnText: true)
+        echo json
+        echo "curl -X POST --location \"$webhookUrl\" -H \"Content-Type: application/json\" -d '${json}'"
         sh("curl -X POST --location \"$webhookUrl\" -H \"Content-Type: application/json\" -d '${json}'")
     }
 }
