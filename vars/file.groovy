3de154d9c94130e206859d34d00786df5898be56
miha <mihanvr@gmail.com>
2025-07-08 16:36:14 +0300

    rework delete artifact

--- vars/file.groovy
+++ vars/file.groovy
@@ -1,11 +1,11 @@
 def deleteFile(String path) {
     if (isUnix()) {
         def status = sh script: "rm -f '${path}'", returnStatus: true
-        log.info("delete status: ${status}")
+        log.info("delete status: ${status}, type: ${status.getClass()}")
         return status == 0
     } else {
         def status = bat script: "del /F /Q \"${path}\"", returnStatus: true
-        log.info("delete status: ${status}")
+        log.info("delete status: ${status.getClass()}")
         return status == 0
     }
 }
